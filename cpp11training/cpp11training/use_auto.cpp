fed49b5fff5662593e57721e50eedd56bd32627e
Kristoffel Pirard <kristoffel.pirard@tomra.com>
2016-01-11 11:35:49 +0100

    [impl] add 'Language' concept requirement

--- cpp11training/cpp11training/use_auto.cpp
+++ cpp11training/cpp11training/use_auto.cpp
@@ -7,21 +7,41 @@
 #include <string>
 
 // CODE UNDER TEST:
-int find_number(const std::string & name)
-{
-	const std::vector<std::map<std::string, int>> mappings{
-		{ { "een", 1 },{ "twee", 2 } },
-		{ { "one", 1 },{ "two", 2 } }
-	};
+using Language = std::string;
+using Word = std::string;
+using Number = int;
 
+std::map<Language, std::map<Word, Number>> mappings{
+	{ "nl-nl",{ { "een", 1 },{ "twee", 2 } } },
+	{ "en-en",{ { "one", 1 },{ "two", 2 } } }
+};
+
+int find_number(const Word & name)
+{
 	return 0;
 }
 
-TEST(auto_usage, we_can_limit_the_amount_of_typing)
+int translate(const Language &language, const Word & name)
+{
+	return 0;
+}
+
+void set_entry(const Language &language, Word word, int i)
+{
+}
+
+TEST(find_number, we_can_just_auto_translate)
 {
 	EXPECT_EQ(1, find_number("een"));
 	EXPECT_EQ(2, find_number("twee"));
 	EXPECT_EQ(1, find_number("one"));
 	EXPECT_EQ(2, find_number("two"));
 	EXPECT_THROW(find_number("zork"), std::range_error);
+}
+
+
+TEST(change_number, we_can_change_entries)
+{
+	EXPECT_NO_THROW(set_entry("en-en", "three", 3));
+	EXPECT_EQ(3, translate("en-en", "three"));
 }
