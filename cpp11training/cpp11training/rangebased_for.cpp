6ebe5a07d08ef10be03525feb6b26ea1e49e5813
Kristoffel Pirard <kristoffel.pirard@tomra.com>
2016-01-11 15:13:22 +0100

    [impl] range-based for exercise

---
+++ cpp11training/cpp11training/rangebased_for.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+class Range {
+public:
+
+    Range(int first, int after_last)
+        : first{ first }
+        , after_last{ after_last }
+    {}
+
+    int first;
+    int after_last;
+};
+
+// TODO: make this compile
+TEST(range_based_for, we_can_iterate_over_a_collection)
+{
+    int result = 0;
+#ifdef solving_this
+    for (const auto &element : Range{ 1, 10 })
+    { 
+        result += element;
+    }
+#endif
+    EXPECT_EQ(90, result);
+}
