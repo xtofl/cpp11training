163149260c169b226944adc3bb83f89595a9dbf1
Kristoffel Pirard <kristoffel.pirard@tomra.com>
2016-01-11 16:21:27 +0100

    [impl] exercise for generic range-based for

--- cpp11training/cpp11training/rangebased_for.cpp
+++ cpp11training/cpp11training/rangebased_for.cpp
@@ -4,6 +4,42 @@
 
 #include <string>
 
+TEST(range_based_for, rewrite_to_cpp11)
+{
+    std::vector<int> ints{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int result = 0;
+    for (std::vector<int>::const_iterator it = ints.begin(); it != ints.end(); ++it)
+    {
+        result += *it;
+    };
+    EXPECT_EQ(45, result);
+}
+
+
+template<class Container, class result_type = Container::value_type>
+result_type sum(const Container &c)
+{
+    result_type result = 0;
+    typename Container::const_iterator it = c.begin();
+    const typename Container::const_iterator itEnd = c.end();
+    for (; it != itEnd; ++it)
+    {
+        result += *it;
+    }
+    return result;
+}
+
+TEST(range_based_for, use_on_generic_containers_also_arrays)
+{
+    std::vector<int> ints{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    EXPECT_EQ(45, sum(ints) );
+// TODO: make this compile (and run...)
+#if working_on_this
+    long longs[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    EXPECT_EQ(45, sum(longs));
+#endif
+}
+
 class Range {
 public:
 
