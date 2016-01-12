638bddda7ccccbcac8e8f3698b3a64bfbcc9ab4b
Kristoffel Pirard <kristoffel.pirard@tomra.com>
2016-01-12 07:09:48 +0100

    [impl] enum exercise

---
+++ cpp11training/cpp11training/enums.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "gtest/gtest.h"
+
+enum A { letter, number };
+
+//TODO fefine it
+#ifdef using_enum_classes
+enum B { newspaper, letter };
+#endif
+
+TEST(enums, enums_are_not_confusing)
+{
+    A character = number;
+    switch (character) {
+    case letter:
+        FAIL(); break;
+
+//TODO: make it compile
+#ifdef using_enum_classes
+    case number:
+        SUCCEED(); break;
+#endif
+    }
+}
+
+TEST(enums, the_size_is_under_control)
+{
+    EXPECT_EQ(8, sizeof(A));
+}
