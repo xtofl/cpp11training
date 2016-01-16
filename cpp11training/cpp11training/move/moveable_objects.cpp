3952690ddc5c2681cb48890f6af5b625190f1233
Kristoffel Pirard <kristoffel.pirard@tomra.com>
2016-01-16 17:33:18 +0100

    [impl] move ownership exercise

--- cpp11training/cpp11training/move/moveable_objects.cpp
+++ cpp11training/cpp11training/move/moveable_objects.cpp
@@ -1,6 +1,9 @@
 #include "stdafx.h"
 
 #include <gtest/gtest.h>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 #include "perftest.h"
 
@@ -35,4 +38,55 @@ TEST(move, speed_this_up)
     EXPECT_LE(
         d,
         std::chrono::milliseconds(1000)) << d;
+}
+
+
+
+TEST(move, there_should_be_only_one_owner)
+{
+    class Resource {
+    public:
+        Resource(std::string id)
+            : id(std::move(id))
+        {}
+
+        std::string id;
+    };
+
+    class Pool {
+    public:
+        Pool()
+            : resources{ {Resource{"one"}, Resource{"two"}} }
+        {}
+        size_t size() const {
+            return resources.size();
+        }
+        
+        Resource borrow() {
+            return Resource{ "?" };
+        }
+        
+        void return_(Resource) {}
+
+        bool contains(const std::string &id) const
+        {
+            return std::any_of(begin(resources), end(resources),
+                [&](const auto &resource) { return resource.id == id; });
+        }
+    private:
+        std::vector<Resource> resources;
+    };
+
+
+    Pool pool;
+    auto r1 = pool.borrow();
+    EXPECT_EQ("one", r1.id);
+    EXPECT_EQ(1u, pool.size());
+    EXPECT_FALSE(pool.contains("one"));
+    EXPECT_TRUE(pool.contains("two"));
+
+    pool.return_(r1);
+    EXPECT_EQ(2u, pool.size());
+    EXPECT_TRUE(pool.contains("one")); 
+    EXPECT_TRUE(pool.contains("two"));
 }
