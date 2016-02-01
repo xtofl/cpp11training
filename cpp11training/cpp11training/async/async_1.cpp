5ee7e93fcc9520c91257640476ddf83939793758
Kristoffel Pirard <kristoffel.pirard@tomra.com>
2016-02-01 15:01:58 +0100

    [impl] use event queue to assert order in tests

--- cpp11training/cpp11training/async/async_1.cpp
+++ cpp11training/cpp11training/async/async_1.cpp
@@ -11,29 +11,84 @@ std::chrono::milliseconds operator"" _ms(unsigned long long count) {
     return std::chrono::milliseconds(count);
 }
 
-namespace www {
+class AsyncTest : public ::testing::Test {
+public:
+    AsyncTest()
+    {}
 
-    auto get_web_page(std::string url)
+    class Events {
+    public:
+        struct Event {
+            std::string source;
+            std::string message;
+            bool operator == (const Event &other) const {
+                return source == other.source && message == other.message;
+            }
+        };
+        void push(Event s) {
+            std::lock_guard<std::mutex> guard(mutex);
+            events.emplace_back(std::move(s));
+        }
+
+        int index(const Event &e) {
+            std::lock_guard<std::mutex> guard(mutex);
+            auto it = std::find(std::begin(events), end(events), e);
+            return distance(std::begin(events), it);
+        }
+
+        std::mutex mutex;
+    private:
+        std::vector<Event> events;
+    };
+
+    Events events;
+
+    auto get(std::string url)
     {
+        events.push({"get: "+url, "entry"});
         std::this_thread::sleep_for(1000_ms);
+        events.push({"get: " + url, "returning" });
         return "results for " + url;
+    }
+
+    template<class ...Args>
+    auto post(std::string url, const Args... args)
+    {
+        events.push({ "post: " + url, "entry" });
+        std::this_thread::sleep_for(1000_ms);
+        events.push({ "post: " + url, "exit" });
+        return "posted to " + url;
     }
 
     template<class Urls>
     auto get_parallel(const Urls &urls)
     {
+        events.push({ "get_parallel: ", "entry" });
         for (const auto &url : urls)
         {
-            www::get_web_page(url);
+            get(url);
         }
+        events.push({ "get_parallel: ", "exit" });
+        return 0;
     }
 
-    const std::string urls[] = {
+    const std::vector<std::string> urls{
         "http://google.com", "http://yahoo.com", "http://sioux.eu"
     };
+};
+
+TEST_F(AsyncTest, DISABLED_we_can_delegate_stuff)
+{
+    EXPECT_GT(2 * 1000_ms, duration([=] {get_parallel(urls); }, 1));
 }
 
-TEST(async_1, DISABLED_we_can_delegate_stuff)
+TEST_F(AsyncTest, DISABLED_we_can_wait_for_delegated_stuff)
 {
-    EXPECT_GT(2 * 1000_ms, duration([] {www::get_parallel(www::urls); }, 1));
+    auto google = get("http://google.com");
+    auto correct = post("http://spell_checker.com", google);
+
+    //EXPECT_TRUE(correct);
+    EXPECT_LT(
+        events.index({ "get: http://google.com", "exit" }),
+        events.index({ "post: http://spell_checker.com", "entry" }));
 }
