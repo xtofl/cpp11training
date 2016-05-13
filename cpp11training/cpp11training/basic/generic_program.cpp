cf232498b69bf7917b01496c42b6b1202c427778
xtofl <xtofl@github.com>
2016-05-13 10:54:01 +0200

    [impl] extract "Partial Specialization" exercise

--- cpp11training/cpp11training/basic/generic_program.cpp
+++ cpp11training/cpp11training/basic/generic_program.cpp
@@ -15,9 +15,28 @@ enum class Type {
 
 TEST(generic, algorithmCanUseManyInputTypes)
 {
-	EXPECT_EQ(Type::String, getTypeFor("abc"));
+	auto s = "abc";
+
+	EXPECT_EQ(Type::String, getTypeFor(s);
 	EXPECT_EQ(Type::String, getTypeFor(std::string{});
 	EXPECT_EQ(Type::Number, getTypeFor(1.0));
 	EXPECT_EQ(Type::Number, getTypeFor(sizeof(""));
+}
+#endif
+
+// TODO
+// #define I_KNOW_GENERICS_AND_PARTIAL_SPECIALIZATION
+//
+// and make the tests compile and succeed
+
+#ifdef I_KNOW_GENERICS_AND_PARTIAL_SPECIALIZATION
+TEST(generic, algorithmEvenKnowsArrayTypes)
+{
+	int is [5];
+	EXPECT_EQ(Type::Array, getTypeFor(is));
+}
+TEST(generic, algorithmEvenKnowsArrayTypesUnlessForChars)
+{
+	EXPECT_EQ(Type::String, getTypeFor("abc"));
 }
 #endif
