7f5968b4a682cdd9b3373caf13a79f256bbfbaa0
xtofl drarip <xtofl@hotmail.com>
2017-05-22 15:24:11 +0200

    [doc] clarify lambda exercises

--- cpp11training/cpp11training/lambdas.cpp
+++ cpp11training/cpp11training/lambdas.cpp
@@ -9,7 +9,9 @@
 #include <exception>
 
 // should override these with local lambdas
+// don't touch these functions...
 void foo() { throw std::runtime_error{ "not implemented" }; }
+int bar() { return 1; }
 int length(const std::string &s) { return 100; }
 
 TEST(lambdas, DISABLED_we_can_define_local_lambdas)
@@ -17,6 +19,7 @@ TEST(lambdas, DISABLED_we_can_define_local_lambdas)
 	// TODO: create a lambda that returns the length
 	// of a string, and a `foo` that does just nothing
 	//
+    // GOAL: get acquainted with the lambda expression syntax
     EXPECT_EQ(3, length("abc"));
     EXPECT_NO_THROW(foo());
 }
@@ -25,13 +28,24 @@ TEST(lambdas, DISABLED_we_can_capture_the_local_variables_by_value)
 {
     for (int i = 0; i != 10; ++i)
     {
-        EXPECT_EQ(i, length("abc"));
+        // TODO: alter make_bar to return
+        // different functions on every iteration
+        //
+        // GOAL: lambda expressions can be used
+        // to create functions on-the-fly!
+        auto make_bar = [] { return bar; };
+
+        auto bar = make_bar();
+        EXPECT_EQ(i, bar());
     }
 }
 
 TEST(lambdas, DISABLED_we_can_capture_local_variables_by_reference)
 {
     int receiver = 0;
+    // TODO: create a local lambda expression that
+    // makes `receiver` equal to `i` every time
+    //
     for (int i = 0; i != 10; ++i)
     {
         foo();
@@ -42,6 +56,11 @@ TEST(lambdas, DISABLED_we_can_capture_local_variables_by_reference)
 
 TEST(lambdas, DISABLED_we_can_add_state)
 {
+    // TODO:
+    // create a lambda expression `foo` that
+    // returns a different value upon subsequent calls
+    //
+    // HINT: lambda expressions are immutable, unless...
     int foo_calls = 0;
     foo();
     EXPECT_EQ(1, foo_calls);
@@ -66,7 +85,7 @@ TEST(lambdas, DISABLED_we_can_add_state)
 #include "class_design/MyBike.h"
 #include "class_design/Piano.h"
 
-// TODO: convert this into a map<key, lambda>
+// TODO: implement this with a map<key, factory function>
 std::unique_ptr<Thing> make(const std::string &what)
 {
     return nullptr;
@@ -74,6 +93,11 @@ std::unique_ptr<Thing> make(const std::string &what)
 
 TEST(lambdas, DISABLED_we_can_instantiate_based_on_a_typename)
 {
+    // TODO: alter the `make` function to return an object
+    // of the type specified by the provided argument.
+    //
+    // HINT: a std::map is great for lookup, but is not polymorphic.
+    // a std::function can wrap any lambda expression
     auto f = make("my bike");
     EXPECT_FALSE(dynamic_cast<MyBike*>(f.get()) == nullptr);
     auto i = make("piano");
@@ -81,7 +105,6 @@ TEST(lambdas, DISABLED_we_can_instantiate_based_on_a_typename)
 }
 
 
-// TODO: make 
 std::function<double(double)> make_adder(double operand)
 {
     return std::function<double(double)>{};
@@ -89,6 +112,11 @@ std::function<double(double)> make_adder(double operand)
 
 TEST(lambdas, DISABLED_we_can_instantiate_functions_at_runtime)
 {
+    // TODO: alter make_adder so that it can be used
+    // to create different functions, depending on the argument
+    //
+    // GOAL: an entry-level functional programming exercise
+    //
     auto add5 = make_adder(5.0);
     EXPECT_NEAR(5.0, add5(0.0), 0.001);
     EXPECT_NEAR(15.0, add5(10.0), 0.001);
@@ -98,52 +126,33 @@ TEST(lambdas, DISABLED_we_can_bind_arguments)
 {
     auto add5 = std::plus<int>();
 //TODO: #define we_can_bind_an_argument
+// and define add5 with std::bind
+//
+// HINT: use a placeholder!
 #ifdef we_can_bind_an_argument
     EXPECT_EQ(5, add5(0));
     EXPECT_EQ(15, add5(10));
 #endif
 }
 
-std::function<double(double)> make_safe(std::function<double(double)> unsafe_function, std::function<bool(double)> check)
+std::function<double(double)> make_safe(
+    std::function<double(double)> unsafe_function,
+    std::function<bool(double)> check)
 {
     return std::function<double(double)>{};
 }
 
 TEST(lambdas, DISABLED_we_can_add_a_policy_to_a_function)
 {
-    const auto &reciproc = [](double f) { return 1. / f; };
-    
-    std::string status;
+    const auto reciproc = [](double f) { return 1. / f; };
+
     const auto &safe_reciproc = make_safe(reciproc, [&](double f) {
-        if (f == 0.0) {
-            status = "division by zero";
-            return false;
-        }
-        else
-        {
-            return true;
-        }
+        return (f != 0.0);
     });
-    EXPECT_NO_THROW(safe_reciproc(0.0));
-    EXPECT_EQ("division by zero", status);
-}
-
-TEST(lambdas, DISABLED_take_care_of_object_lifetimes)
-{
-    std::function<std::vector<int>()> read;
-    {
-        std::vector<int> i{ {1, 2, 3, 4} };
-        read = [&] { return i; };
-    }
-    EXPECT_EQ(4u, read().size());
-
-    {
-        std::vector<int> i{ {1, 2, 3, 4} };
-        read = [&] { return i; };
-        auto j = std::move(i);
-        EXPECT_EQ(4u, read().size());
-    }
-
+    // TODO: alter `make_safe` so that it throws runtime_error when
+    // its policy returns false, and otherwise calls the provided function
+    EXPECT_NEAR(2.0, safe_reciproc(0.5), 1e-5);
+    EXPECT_THROW(safe_reciproc(0.0), std::runtime_error);
 }
 
 
