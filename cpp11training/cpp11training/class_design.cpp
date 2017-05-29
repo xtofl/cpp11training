3b49e08fe61b4e1f03ccde4364f2b0364e9758da
xtofl drarip <xtofl@hotmail.com>
2017-05-29 10:39:54 +0200

    clarify class-design exercise

--- cpp11training/cpp11training/class_design.cpp
+++ cpp11training/cpp11training/class_design.cpp
@@ -8,16 +8,20 @@
 #include "class_design/Vehicle.h"
 #include "class_design/MyBike.h"
 
-// make the compiler complain when we change the Thing::size_in_cm to size_in_m
+// TODO:
+// visit Thing.h, and rename Thing::size_in_cm to size_in_m.
+// The code still compiles.  That's bad.  Make it so that inheritors
+// don't compile by accident.
+//
+// HINT: use the `override` keyword
 
-// make it trivial to add a constructor Thing::Thing(std::string name)
 TEST(class_design, DISABLED_adding_constructors_should_be_trivial)
 {
     // TODO: add a constructor Thing(std::string)
     // make Piano and MyBike inherit this constructor with the least amount of code
     // and replication
     //
-    // GOAL: a lean class hierarchy
+    // GOAL: make it trivial to add a base class constructors like Thing::Thing(std::string name)
     //
     // HINT: `using` is the key
     Piano p; // { "my piano" };
@@ -30,7 +34,8 @@ TEST(class_design, DISABLED_adding_constructors_should_be_trivial)
 	EXPECT_NEAR(60.0, b.size_in_cm(), 0.00001);
 }
 
-// make it impossible to copy things
+// TODO: alter the needed classes to make it impossible to copy things
+// GOAL: learn to use `delete` decorator
 TEST(class_design, DISABLED_synthesized_functions)
 {
     Piano p; // { "" };
--- cpp11training/cpp11training/class_design/Thing.h
+++ cpp11training/cpp11training/class_design/Thing.h
@@ -6,7 +6,14 @@
 class Thing {
 public:
 	virtual ~Thing() {}
-	virtual double size_in_cm() const = 0;
+
+    virtual double size_in_cm() const
+        // TODO: just change the method name
+        // and make sure the derived classes don't accidentally
+        // compile.
+    //virtual double size_in_m() const
+    { throw std::logic_error("not implemented"); }
+
 	virtual std::string purpose() const = 0;
 	
 	virtual std::string name() const { return "just a thing"; }
