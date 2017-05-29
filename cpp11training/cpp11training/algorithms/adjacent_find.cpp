ac7d41f9b383a532cff91a73a70380c68b643c09
xtofl drarip <xtofl@hotmail.com>
2017-05-29 07:33:24 +0200

    document adjacent_find exercise

--- cpp11training/cpp11training/algorithms/adjacent_find.cpp
+++ cpp11training/cpp11training/algorithms/adjacent_find.cpp
@@ -4,14 +4,6 @@
 #include <vector>
 #include <unordered_map>
 
-const auto make_f = [](auto a, auto b, auto c) {
-    return [=](auto x) {
-        // https://www.desmos.com/calculator/trpwba2fej
-        auto t = (x / 10.0 / b - 10.0 * a);
-        return t*t*t - 8 * t*t - 13 * t + c;
-    };
-};
-
 const auto samples = [](auto f)
 {
     std::vector<int> values(1000);
@@ -22,23 +14,40 @@ const auto samples = [](auto f)
     return values;
 };
 
-TEST(adjacent_find, DISABLED_where_is_the_first_top)
-{
-    const auto values = samples(make_f(1.1, 10, 6));
-
-    // TODO: calculate where the function values remain equal
-    const auto top = std::begin(values);
-
-    EXPECT_EQ(*top, *std::next(top));
-}
-
 
 TEST(adjacent_find, DISABLED_where_is_the_first_zero_crossing)
 {
     const auto values = samples([](auto x) { return x*x*x - 8 * x*x - 13 * x - 4; });
 
     // TODO: calculate where the function values crosses zero
+    // GOAL: learn to use adjacent find (and forget manually chasing pointers)
     const auto zerocrossing = std::begin(values);
 
-    EXPECT_TRUE( (*zerocrossing) * (*std::next(zerocrossing)) <= 0 );
+    EXPECT_TRUE((*zerocrossing) * (*std::next(zerocrossing)) <= 0);
 }
+
+
+
+const auto make_f = [](auto a, auto b, auto c) {
+    return [=](auto x) {
+        // https://www.desmos.com/calculator/trpwba2fej
+        auto t = (x / 10.0 / b - 10.0 * a);
+        return t*t*t - 8 * t*t - 13 * t + c;
+    };
+};
+
+
+TEST(adjacent_find, DISABLED_where_is_the_first_top)
+{
+    const auto values = samples(make_f(1.1, 10, 6));
+
+    // TODO: calculate where the function values remain equal
+    // GOAL: learn to use separate iteration from predicate
+    // HINT: 'equal' may means 'close enough to one another', i.e.
+    // you may cast to integer.
+    // HINT2: the result will be around 10 somewhere.
+    const auto top = std::begin(values);
+
+    EXPECT_EQ(*top, *std::next(top));
+}
+
