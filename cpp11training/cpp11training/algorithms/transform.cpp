8b9cd8c0662886b04c01055c02c9510f1547f0af
xtofl drarip <xtofl@hotmail.com>
2017-05-29 09:20:31 +0200

    document transform exercise

--- cpp11training/cpp11training/algorithms/transform.cpp
+++ cpp11training/cpp11training/algorithms/transform.cpp
@@ -4,13 +4,13 @@
 #include <vector>
 #include <unordered_map>
 
-// TODO: use algorithms to calculate the square of each element
 TEST(apply_transform, DISABLED_basic_arithmetic)
 {
     const std::vector<int> xs{ {2, 5, 10, 40} };
 
     std::vector<int> squares;
 
+    // TODO: use algorithms to calculate the square of each element
     ASSERT_EQ(xs.size(), squares.size());
     EXPECT_EQ(4, squares[0]);
     EXPECT_EQ(25, squares[1]);
@@ -23,6 +23,9 @@ TEST(apply_transform, DISABLED_string_representation)
     const std::vector<int> xs{ { 2, 5, 10, 40 } };
 
     std::vector<std::string> squares;
+    // TODO: use algorithms to transform xs into the
+    // string representation of their squares
+    // GOAL: use a more complex transformation
 
     ASSERT_EQ(xs.size(), squares.size());
     EXPECT_EQ("4", squares[0]);
@@ -42,6 +45,8 @@ TEST(apply_transform, DISABLED_extract_map_keys)
     }};
 
     std::vector<std::string> keys;
+    // TODO: extract the keys of the `numbers` map
+    // using a `transform`
 
     ASSERT_EQ(numbers.size(), keys.size());
     EXPECT_EQ("two", keys[0]);
@@ -58,7 +63,10 @@ TEST(apply_transform, DISABLED_join_two_input_ranges)
     const std::vector<int> values{ {2, 5, 10, 40} };
 
     std::vector<std::string> numbers;
-
+    // TODO: transform keys and values into
+    // this python-key-value syntax
+    // GOAL: see that `transform` can accept two ranges
+    // and 'zip' them together
     ASSERT_EQ(keys.size(), numbers.size());
     EXPECT_EQ("two: 2", numbers[0]);
     EXPECT_EQ("five: 5", numbers[1]);
