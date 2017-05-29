f5da0e6444673bcf0cdd64cd83f3dbd1670cc062
xtofl drarip <xtofl@hotmail.com>
2017-05-29 10:39:54 +0200

    clarify unique_ptr exercise

--- cpp11training/cpp11training/resource_management/unique_ptr.cpp
+++ cpp11training/cpp11training/resource_management/unique_ptr.cpp
@@ -2,6 +2,7 @@
 
 #include <gtest/gtest.h>
 #include <memory>
+#include <array>
 #include <functional>
 
 class JarPotVessel {
@@ -17,6 +18,8 @@ TEST(unique_ptr, DISABLED_object_is_destructed_when_ptr_out_of_scope)
 {
 	// TODO: make `thing_wrapper` 'own' the object,
 	// to prevent it from leaking
+    // GOAL: learn to use unique_ptr to express ownership of
+    // objects on the free store.
     auto thing_wrapper = new JarPotVessel();
     bool destructed = false;
     thing_wrapper->destructed = [&] { destructed = true; };
@@ -31,20 +34,24 @@ TEST(unique_ptr, DISABLED_object_is_destructed_when_ptr_out_of_scope)
 
 TEST(unique_ptr, DISABLED_cant_copy_a_unique_ptr)
 {
-//TODO #define i_know_what_is_wrong
+// TODO:
+// #define i_know_what_is_wrong
+// and make the code compile:
+// a vector of owners can't be copied, since then there would be
+// two owners of each contained object.
 #ifdef i_know_what_is_wrong
-    std::vector<std::unique_ptr<int>> values{ {
+    std::array<std::unique_ptr<int>, 2> values {
             std::make_unique<int>(1),
             std::make_unique<int>(2),
-        }
     };
 
     EXPECT_EQ(1, *values.at(0));
     EXPECT_EQ(2, *values.at(1));
 
-    auto copy = values;
+    // TODO: tinker with this line to let the test pass
+    auto target = values;
 
-    EXPECT_EQ(1, *copy.at(0));
-    EXPECT_EQ(2, *copy.at(1));
+    EXPECT_EQ(1, *target.at(0));
+    EXPECT_EQ(2, *target.at(1));
 #endif
 }
