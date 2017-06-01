d8a727e0fb720863ba7d99ea2cf85b80ca2bdb8e
xtofl drarip <xtofl@hotmail.com>
2017-06-01 11:53:02 +0200

    add harder exercise

--- cpp11training/cpp11training/variadic_templates.cpp
+++ cpp11training/cpp11training/variadic_templates.cpp
@@ -1,9 +1,10 @@
 #include <gtest/gtest.h>
 #include <string>
+#include <vector>
 
 // TODO: alter a function template to return the number of arguments passed.
 // GOAL: be able to specify a variadic function template and use the sizeof... operator
-
+// GRADE: ENTRY
 // #define i_can_specify_a_variadic_function_template
 #ifdef i_can_specify_a_variadic_function_template
 template<typename T1 = int, typename T2 = int, typename T3 = int>
@@ -19,7 +20,7 @@ TEST(variadic, we_can_count_arguments)
 
 // TODO: alter the class template to know its number of arguments
 // GOAL: be able to specify a variadic class template and use the sizeof... operator
-
+// GRADE: ENTRY
 //#define i_can_specify_a_variadic_class_template
 #ifdef i_can_specify_a_variadic_class_template
 
@@ -46,10 +47,66 @@ namespace {
 }
 TEST(serialization, DISABLED_serialize_different_types)
 {
-    // TODO: create serialize so that the test passes
+    // TODO: fill in serialize so that the test passes
     // GOAL: get familiar with template parameter packs
+    // GOAL: learn how to expand a (single) parameter pack
+    // GRADE: INTERMEDIATE
     EXPECT_EQ("", serialize());
     EXPECT_EQ("int@1", serialize(1));
     EXPECT_EQ("string@'abc'", serialize("abc"));
     EXPECT_EQ("int@1, string@'abc'", serialize(1, "abc"));
+}
+
+// TODO: implement flatten to allow polymorphic concatenation
+// GOAL: learn to iteration from dispatching
+// HINT: the base case is a single vector
+// GRADE: INTERMEDIATE
+template<typename ...Ts>
+auto flatten(Ts... ts) {
+    return std::vector<int>();
+}
+TEST(variadic_monad, DISABLED_flatten_operation)
+{
+    EXPECT_EQ((std::vector<int>{ 1, 4, 9 }),
+        (flatten(std::vector<int>({ 1 }), 2, std::vector<int>{ 3, 4, 5 })));
+}
+
+// this exercise will take some more time...
+// 
+// TODO: fill in the `product` function so that it prints a table
+// of the functions applied to the arguments
+// GOAL: learn to deal with multiple packs and expansions
+// GRADE: HARD
+template<typename ...Fs, typename ...Ts>
+std::string product(std::tuple<Fs...> functions, std::tuple<Ts...> arguments)
+{
+    return "";
+}
+
+TEST(composition, DISABLED_print_a_matrix)
+{
+    const auto table = product(
+        std::make_tuple(
+            [](auto i) { return i; },
+            [](auto i) { return i*i; },
+            [](auto i) { return i*i - 1; }
+            ),
+        std::make_tuple(1, 2, 3)
+    );
+    EXPECT_EQ(R"(
+1, 2, 3
+1, 4, 9
+0, 7, 26
+)", table);
+
+    EXPECT_EQ(R"(
+1, 4, 9, 100
+0, 7, 26, 1000
+)", (product(
+    std::make_tuple(
+        [](auto i) { return i*i; },
+        [](auto i) { return i*i*i; }
+        ),
+    std::make_tuple(1, 2, 3, 10)
+)));
 }
