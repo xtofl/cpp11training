dfdd2791127cd4fce0eb2bbebecf98137b168157
xtofl <kristoffel.pirard+git@gmail.com>
2018-05-12 09:14:19 +0200

    [impl] number exercises for the 'core extensions' presentation
    So that it is easier to find your way around

--- cpp11training/cpp11training/6.types_and_metaprogramming.cpp
+++ cpp11training/cpp11training/6.types_and_metaprogramming.cpp
@@ -8,6 +8,9 @@ TT add(T1 t1, T2 t2) {
     return result;
 }
 
+// GOAL: learn to build 'compiler errors' for your users
+// with static_assert
+//
 TEST(static_assert_, DISABLED_add_should_only_work_with_numbers)
 {
     // this code should compile
