859693adc1b08f37f78911a39eef20bb6fd575ce
xtofl <kristoffel.pirard+git@gmail.com>
2018-06-26 10:47:52 +0200

    clarify exercise

--- cpp11training/cpp11training/6.types_and_metaprogramming.cpp
+++ cpp11training/cpp11training/6.types_and_metaprogramming.cpp
@@ -8,8 +8,15 @@ TT add(T1 t1, T2 t2) {
     return result;
 }
 
+// TODO: add a static_assert to the `add` function
+// so that the following code fails with a comprehensible error
+// message "add is not allowed for non-numeric types"
+//
 // GOAL: learn to build 'compiler errors' for your users
 // with static_assert
+//
+// NOTE: of course, you'll need to undo your changes in order
+// to make the project compile.
 //
 TEST(static_assert_, DISABLED_add_should_only_work_with_numbers)
 {
