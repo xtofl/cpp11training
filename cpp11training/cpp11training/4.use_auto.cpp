2a0757cb61f333091dd1583066507dc91a78acb4
xtofl <kristoffel.pirard+git@gmail.com>
2018-06-26 10:43:29 +0200

    wording; tabs -> spaces

--- cpp11training/cpp11training/4.use_auto.cpp
+++ cpp11training/cpp11training/4.use_auto.cpp
@@ -13,28 +13,29 @@ using Number = int;
 using LanguageLib = std::map<Language, std::map<Word, Number>>;
 
 LanguageLib mappings{
-	{ "nl-nl",{ { "een", 1 },{ "twee", 2 } } },
-	{ "en-en",{ { "one", 1 },{ "two", 2 } } }
+    { "nl-nl",{ { "een", 1 },{ "twee", 2 } } },
+    { "en-en",{ { "one", 1 },{ "two", 2 } } }
 };
 
 
 namespace detail {
-	//TODO: reduce type lenght using deferred function type...
-	template<class Library, class Level1Key, class Level2Key>
-	typename Library::value_type::second_type::value_type::second_type find_two_level(const Library &library, const Level1Key &key1, const Level2Key &key2)
-	{
-		return typename Library::value_type::second_type::value_type::second_type();
-	}
+    // TODO: reduce type verbosity using deferred function type...
+    // GOAL: learn that lengthy types are no longer needed in C++ 11+
+    template<class Library, class Level1Key, class Level2Key>
+    typename Library::value_type::second_type::value_type::second_type find_two_level(const Library &library, const Level1Key &key1, const Level2Key &key2)
+    {
+        return typename Library::value_type::second_type::value_type::second_type();
+    }
 }
 
 int find_number(const Word & name)
 {
-	return 0;
+    return 0;
 }
 
 int translate(const Language &language, const Word & name)
 {
-	return 0;
+    return 0;
 }
 
 void set_entry(const Language &language, Word word, int i)
@@ -44,25 +45,29 @@ void set_entry(const Language &language, Word word, int i)
 TEST(find_number, DISABLED_we_can_just_auto_translate)
 {
     // TODO: enable the test and implement `find_number` to make it succeed
+    //
+    // GOAL: learn to use the `auto` keyword for local variables
+    // and see that complex types are not hard to use
+    //
     // HINT: return the first entry containing a language that defines the given word
     // HINT: use the power of range-based for.
     // HINT: or... more advanced... an stl algorithm with some lambda expressions
-	EXPECT_THROW(find_number("zork"), std::range_error);
-	EXPECT_EQ(1, find_number("een"));
-	EXPECT_EQ(2, find_number("twee"));
-	EXPECT_EQ(1, find_number("one"));
-	EXPECT_EQ(2, find_number("two"));
+    EXPECT_THROW(find_number("zork"), std::range_error);
+    EXPECT_EQ(1, find_number("een"));
+    EXPECT_EQ(2, find_number("twee"));
+    EXPECT_EQ(1, find_number("one"));
+    EXPECT_EQ(2, find_number("two"));
 }
 
 
 TEST(change_number, DISABLED_we_can_change_entries)
 {
-	EXPECT_NO_THROW(set_entry("en-en", "three", 3));
-	EXPECT_EQ(3, translate("en-en", "three"));
+    EXPECT_NO_THROW(set_entry("en-en", "three", 3));
+    EXPECT_EQ(3, translate("en-en", "three"));
 }
 
 
 TEST(change_number, DISABLED_we_can_use_generic_lookup_with_deferred_return_type)
 {
-	EXPECT_EQ(1, detail::find_two_level(mappings, "nl-nl", "een"));
+    EXPECT_EQ(1, detail::find_two_level(mappings, "nl-nl", "een"));
 }
