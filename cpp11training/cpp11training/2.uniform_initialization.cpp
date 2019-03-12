d739aabc99bf41ed3e6a9f52aa0255e3aa6325dd
xtofl <kristoffel.pirard@gmail.com>
2019-03-12 13:52:12 +0100

    Clarify uniform-init exercise

--- cpp11training/cpp11training/2.uniform_initialization.cpp
+++ cpp11training/cpp11training/2.uniform_initialization.cpp
@@ -43,78 +43,49 @@ TEST(uniform_initialization, DISABLED_initialize_a_map)
     EXPECT_EQ('b', ascii.at(66));
 }
 
-
+// TODO: specify the members of this class
+// so that they are properly default-initialized
+//
 struct ProtocolMessage {
     using ConversationId = int;
-    enum class Type { number, text };
+    enum class Type { uninitialized, number, text };
     using Bytes = std::vector<std::uint8_t>;
 
     ConversationId conversation;
     Type type;
     Bytes bytes;
 
-    ProtocolMessage() : conversation(-1), type(Type::number), bytes() {}
+    ProtocolMessage() {}
+    ProtocolMessage(const char *) {}
+    ProtocolMessage(long) {}
 };
-
 
 TEST(uniform_initialization, DISABLED_initialize_an_object)
 {
-    // TODO: initialize the variables to make the expectations pass
-    const ProtocolMessage message;
+    // TODO: specify ProtocolMessage to make the expectations pass
+    // GOAL: learn to use member defaults
+    EXPECT_EQ(ProtocolMessage::Type::uninitialized, ProtocolMessage{}.type);
+    EXPECT_EQ(-1, ProtocolMessage{}.conversation);
+    EXPECT_EQ(0, ProtocolMessage{}.bytes.size());
+
+    // TODO: use uniform initializer syntax to properly initialize
+    // the message
+    const ProtocolMessage message("ab");
+
     EXPECT_EQ(ProtocolMessage::Type::text, message.type);
+    EXPECT_EQ(10, message.conversation);
     ASSERT_EQ(2u, message.bytes.size());
     EXPECT_EQ('a', message.bytes.at(0));
     EXPECT_EQ('b', message.bytes.at(1));
 }
 
-class Connection {
-public:
-    ProtocolMessage receive() { return ProtocolMessage(); };
-    void send(ProtocolMessage) {}
-
-    std::vector<ProtocolMessage> messages;
-};
-
-class Peer {
-public:
-    void send(std::string text){
-        ProtocolMessage message;
-        connection->send(message);
-    }
-    void send(int number) {
-        ProtocolMessage message;
-        connection->send(message);
-    }
-
-    std::string receiveText() { return ""; }
-    int receiveNumber() { return 10; }
-private:
-    std::shared_ptr<Connection> connection;
-};
-
-TEST(uniform_initialization, DISABLED_initialize_a_class_object)
-{
-    // TODO: Adapt the `Peer` class and initialize the variables in the test
-    //
-    // GOAL: learn to use brace initialization in different contexts:
-    // - member initializers
-    // - function arguments
-    // - return values
-    // - local variables
-    auto connection = std::make_shared<Connection>();
-    Peer peer;
-    peer.send(10);
-    ASSERT_EQ(1u, connection->messages.size());
-    EXPECT_EQ(ProtocolMessage::Type::number, connection->messages.back().type);
-    ProtocolMessage response;
-    connection->messages.emplace_back(std::move(response));
-    const auto number = peer.receiveNumber();
-    EXPECT_EQ(20, number);
-}
-
-//TODO #define we_can_use_an_initializer_list
-// and make the code compile and the test succeed.
-// by adding an initializer list argument
+// TODO #define we_can_use_an_initializer_list
+//   and make the code compile and the test succeed.
+//   by adding an initializer list argument
+//
+// GOAL:
+//   learn to create functions accepting an initializer list
+//
 // NOTE: a variadic template would be better.
 #ifdef we_can_use_an_initializer_list
 std::string concatenate(...) {
